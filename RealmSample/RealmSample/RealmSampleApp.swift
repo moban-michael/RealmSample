e71d01302251eedd83c860e9c1593f241ef8d84c
Moban Michael <moban.michael@yotta.co.uk>
2020-11-27 17:17:01 +0000

    Base project added

---
+++ RealmSample/RealmSample/RealmSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmSampleApp.swift
+//  RealmSample
+//
+//  Created by Moban Michael on 27/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct RealmSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
